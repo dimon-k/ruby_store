47e9966d5013b4c4028c8871594ae9bc6da23c40
Dmitriy Kanununnikov <dmitriy.kanunnikov@gmail.com>
2019-12-08 18:48:07 +0100

    Refactor existing tests and add two method tests

--- test/checkout_test.rb
+++ test/checkout_test.rb
@@ -10,24 +10,34 @@ class CheckoutTest < Test::Unit::TestCase
     @pricing_rules = { green_tea_promo: true, strawberries_promo: true}
   end
 
+  def test_scan
+    checkout = scan_checkout_items(['GR1'])
+    assert_equal(1, checkout.checkout_items.count)
+  end
+
+  def test_total
+    checkout = scan_checkout_items(['CF1'])
+    assert_equal(11.23, checkout.total)
+  end
+
   def test_green_tea_promo_and_other_items
-    items_product_codes = ['GR1', 'SR1', 'GR1', 'GR1', 'CF1']
-    assert_equal(22.45, scan_checkout_items(items_product_codes))
+    checkout = scan_checkout_items(['GR1', 'SR1', 'GR1', 'GR1', 'CF1'])
+    assert_equal(22.45, checkout.total)
   end
 
   def test_green_tea_promo
-    items_product_codes = ['GR1', 'GR1']
-    assert_equal(3.11, scan_checkout_items(items_product_codes))
+    checkout = scan_checkout_items(['GR1', 'GR1'])
+    assert_equal(3.11, checkout.total)
   end
 
   def test_strawberries_promo
-    items_product_codes = ['SR1', 'SR1', 'GR1', 'SR1']
-    assert_equal(16.61, scan_checkout_items(items_product_codes))
+    checkout = scan_checkout_items(['SR1', 'SR1', 'GR1', 'SR1'])
+    assert_equal(16.61, checkout.total)
   end
 
   def test_coffee_promo
-    items_product_codes = ['GR1', 'CF1', 'SR1', 'CF1', 'CF1']
-    assert_equal(30.57, scan_checkout_items(items_product_codes))
+    checkout = scan_checkout_items(['GR1', 'CF1', 'SR1', 'CF1', 'CF1'])
+    assert_equal(30.57, checkout.total)
   end
   
   private
@@ -35,6 +45,6 @@ class CheckoutTest < Test::Unit::TestCase
   def scan_checkout_items(items_codes)
     checkout = Checkout.new(@pricing_rules)
     items_codes.each { |code| checkout.scan(Product.find(product_code: code)) }
-    checkout.total
+    checkout
   end
 end
