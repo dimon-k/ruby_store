828f642cdb8011f49f8d8c3cb4817c7f5b84024d
Dmitriy Kanununnikov <dmitriy.kanunnikov@gmail.com>
2019-12-08 17:46:34 +0100

    Change database configs to use migrations

--- config/application.rb
+++ config/application.rb
@@ -5,4 +5,9 @@ db_config_file = File.join(File.dirname(__FILE__), 'database.yml')
 if File.exist?(db_config_file)
   config = YAML.load(File.read(db_config_file))
   DB = Sequel.connect(config)
+  Sequel.extension :migration
+end
+
+if DB
+  Sequel::Migrator.run(DB, File.join(File.dirname(__FILE__), '..', 'db', 'migrations'))
 end
